cd732af0673e03626fb5eda5ddc5660e5bca3505
Alexandre Queiroz <xandyzh65@gmail.com>
2024-08-08 17:18:24 -0300

    Movimentação e Lógica de Perseguição
    Foram implementados os comandos para movimentação do personagem, permitindo que o jogador controle suas direções no cenário do jogo. Além disso, foi desenvolvida a lógica de perseguição, na qual o inimigo rastreia o personagem. Caso o inimigo consiga alcançá-lo, o jogo é finalizado com a condição de 'Game Over'.

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ personagem_cor= VERDE
 poder_personagem = 1
 velocidade_personagem = 5
 
-INIMIGO_LARGURA, INIMIGO_ALTURA = 50, 500
+INIMIGO_LARGURA, INIMIGO_ALTURA = 50, 50
 inimigo = pygame.Rect(random.randint(0, LARGURA - INIMIGO_LARGURA), random.randint(0, ALTURA - INIMIGO_ALTURA), INIMIGO_LARGURA, INIMIGO_ALTURA)
 poder_inimigo = poder_personagem + 1
 velocidade_inimigo = 3
@@ -49,3 +49,71 @@ def cria_novo_inimigo():
     poder_inimigo = poder_personagem + 1
     inimigo.x = random.randint(0, LARGURA - INIMIGO_LARGURA)
     inimigo.y = random.randint(0, ALTURA - INIMIGO_ALTURA)
+
+def main():
+    global poder_personagem, item
+    clock =pygame.time.Clock()
+    rodando = True
+    game_over = False
+
+    cria_novo_inimigo()
+
+    while rodando:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                rodando = False
+
+        if not game_over:
+            teclas = pygame.key.get_pressed()
+            if teclas[pygame.K_LEFT]:
+                personagem.x -= velocidade_personagem
+            if teclas[pygame.K_RIGHT]:
+                personagem.x += velocidade_personagem
+            if teclas[pygame.K_UP]:
+                personagem.y -= velocidade_personagem
+            if teclas[pygame.K_DOWN]:
+                personagem.y += velocidade_personagem
+
+            #movimento do inimigo em direção ao personagem
+            if inimigo.x < personagem.x:
+                inimigo.x += velocidade_inimigo
+            elif inimigo.x > personagem.x:
+                inimigo.x -= velocidade_inimigo
+            
+            if inimigo.y < personagem.y:
+                inimigo.y += velocidade_inimigo
+            elif inimigo.y > personagem.y:
+                inimigo.y -= velocidade_inimigo
+
+            if personagem.colliderect(inimigo):
+                if poder_personagem >= poder_inimigo:
+                    print("Você derrotou o inimgo!")
+                    poder_personagem += poder_inimigo
+                    cria_novo_inimigo()
+                else:
+                    game_over = True
+                    print("Game Over")
+            
+            if personagem.colliderect(item):
+                poder_personagem += 2
+                item.x = random.randint(0, LARGURA - ITEM_LARGURA)
+                item.y = random.randint(0, ALTURA - ITEM_ALTURA)
+                print(f"Você encontrou um item! Seu poder agora é {poder_personagem}")
+        
+        desenhar_tudo()
+
+        if game_over:
+            texto_game_over = fonte.render("Game Over", True, BRANCO)
+            tela.blit(texto_game_over, (LARGURA // 2 - texto_game_over.get_width() // 2, ALTURA // 2 - texto_game_over.get_height() // 2))
+            pygame.display.flip()
+            pygame.time.wait(3000)
+            rodando = False
+        
+        clock.tick(30)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
+
+
